409e0dcdb2039b356fd4ee8e48355be905a7f922
jsdawn <kingwyh1993@163.com>
2018-08-03 10:33:53 +0800

    新增身份报表

---
+++ src/views/modules/form/auth.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- TODO: -->
+    <!-- 这里写html代码 -->
+    <h3>身份报表</h3>
+
+    <!-- 新增权限控制按钮 -->
+    <el-button type="primary" v-if="isAuth('form:auth:add')"> 新增 </el-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  // TODO:
+  // 这里写vue的js语法
+}
+</script>
+
+<style scoped>
+/* TODO: */
+/* 这里写相关css样式 */
+
+</style>
